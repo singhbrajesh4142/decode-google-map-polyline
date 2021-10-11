69eca6e5f138401e71d6158e2b0736220e0b4edc
Brajesh Singh <35314485+singhbrajesh4142@users.noreply.github.com>
2021-10-11 21:46:24 +0530

    initial commit
    This module has a feature that can decode your polyline string into a list of ( latitude, longitude ) points

---
+++ maps/src/script/main.js
@@ -0,0 +1,393 @@
+
+var res = {
+  "geocoded_waypoints": [
+    {
+      "geocoder_status": "OK",
+      "place_id": "ChIJDZ17pl0UrjsR5yj_ndgIrwk",
+      "types": ["premise"]
+    },
+    {
+      "geocoder_status": "OK",
+      "place_id": "ChIJkyUILWIUrjsRfsywuWV5iOE",
+      "types": ["street_address"]
+    }
+  ],
+  "routes": [
+    {
+      "bounds": {
+        "northeast": {
+          "lat": 12.9316597,
+          "lng": 77.63751329999999
+        },
+        "southwest": {
+          "lat": 12.9265847,
+          "lng": 77.6285175
+        }
+      },
+      "copyrights": "Map data ©2021",
+      "legs": [
+        {
+          "distance": {
+            "text": "1.3 km",
+            "value": 1312
+          },
+          "duration": {
+            "text": "4 mins",
+            "value": 246
+          },
+          "end_address": "94, Marathahalli - Sarjapur Rd, Sector 2, Jakkasandra, 1st Block Koramangala, HSR Layout 5th Sector, Bengaluru, Karnataka 560034, India",
+          "end_location": {
+            "lat": 12.9265847,
+            "lng": 77.6371577
+          },
+          "start_address": "545, 8th Main Rd, 8th Block, Koramangala 4th Block, Koramangala, Bengaluru, Karnataka 560034, India",
+          "start_location": {
+            "lat": 12.9316597,
+            "lng": 77.6285175
+          },
+          "steps": [
+            {
+              "distance": {
+                "text": "0.2 km",
+                "value": 201
+              },
+              "duration": {
+                "text": "1 min",
+                "value": 32
+              },
+              "end_location": {
+                "lat": 12.9300056,
+                "lng": 77.6292527
+              },
+              "html_instructions": "Head \u003cb\u003esoutheast\u003c/b\u003e on \u003cb\u003e8th Main Rd\u003c/b\u003e toward \u003cb\u003e7th Cross Rd\u003c/b\u003e",
+              "polyline": {
+                "points": "{u|mAgyxxMRGd@SRGf@QXKTG^M^Q`@M^MPENE"
+              },
+              "start_location": {
+                "lat": 12.9316597,
+                "lng": 77.6285175
+              },
+              "travel_mode": "DRIVING"
+            },
+            {
+              "distance": {
+                "text": "0.4 km",
+                "value": 423
+              },
+              "duration": {
+                "text": "1 min",
+                "value": 70
+              },
+              "end_location": {
+                "lat": 12.9298015,
+                "lng": 77.63302139999999
+              },
+              "html_instructions": "Turn \u003cb\u003eleft\u003c/b\u003e onto \u003cb\u003e7th Cross Rd\u003c/b\u003e\u003cdiv style=\"font-size:0.9em\"\u003ePass by SVKM's Narsee Monjee Institute of Management Studies (on the right in 300&nbsp;m)\u003c/div\u003e",
+              "maneuver": "turn-left",
+              "polyline": {
+                "points": "qk|mAy}xxMp@sELy@\\iCD_@LaB?MAQG[Qq@K]_@kA"
+              },
+              "start_location": {
+                "lat": 12.9300056,
+                "lng": 77.6292527
+              },
+              "travel_mode": "DRIVING"
+            },
+            {
+              "distance": {
+                "text": "0.6 km",
+                "value": 565
+              },
+              "duration": {
+                "text": "2 mins",
+                "value": 100
+              },
+              "end_location": {
+                "lat": 12.9275428,
+                "lng": 77.63751329999999
+              },
+              "html_instructions": "Continue onto \u003cb\u003e1st Main Rd\u003c/b\u003e\u003cdiv style=\"font-size:0.9em\"\u003ePass by the medical store (on the left)\u003c/div\u003e",
+              "polyline": {
+                "points": "gj|mAkuyxMMMAQBcAFSBI?ARa@?ATe@DGN[JW@IDINi@@ATk@rAaDj@wANYRq@DGJ]\\o@`@_@v@u@JMLK"
+              },
+              "start_location": {
+                "lat": 12.9298015,
+                "lng": 77.63302139999999
+              },
+              "travel_mode": "DRIVING"
+            },
+            {
+              "distance": {
+                "text": "52 m",
+                "value": 52
+              },
+              "duration": {
+                "text": "1 min",
+                "value": 17
+              },
+              "end_location": {
+                "lat": 12.9272158,
+                "lng": 77.6371757
+              },
+              "html_instructions": "Turn \u003cb\u003eright\u003c/b\u003e toward \u003cb\u003e2nd Main Rd\u003c/b\u003e",
+              "maneuver": "turn-right",
+              "polyline": {
+                "points": "c|{mAmqzxMLJBBDFX\\LJ"
+              },
+              "start_location": {
+                "lat": 12.9275428,
+                "lng": 77.63751329999999
+              },
+              "travel_mode": "DRIVING"
+            },
+            {
+              "distance": {
+                "text": "71 m",
+                "value": 71
+              },
+              "duration": {
+                "text": "1 min",
+                "value": 27
+              },
+              "end_location": {
+                "lat": 12.9265847,
+                "lng": 77.6371577
+              },
+              "html_instructions": "Turn \u003cb\u003eleft\u003c/b\u003e onto \u003cb\u003e2nd Main Rd\u003c/b\u003e\u003cdiv style=\"font-size:0.9em\"\u003eDestination will be on the right\u003c/div\u003e",
+              "maneuver": "turn-left",
+              "polyline": {
+                "points": "cz{mAkozxM\\CRAX@XBXB"
+              },
+              "start_location": {
+                "lat": 12.9272158,
+                "lng": 77.6371757
+              },
+              "travel_mode": "DRIVING"
+            }
+          ],
+          "traffic_speed_entry": [],
+          "via_waypoint": []
+        }
+      ],
+      "overview_polyline": {
+        "points": "{u|mAgyxxMdFiBbBg@bBwLLoBIm@]oA_@kAMM@uAJ]h@kAb@eAl@aB~ByFb@kAPe@\\o@`@_@bAcALKLJHJf@h@p@Er@DXB"
+      },
+      "summary": "7th Cross Rd and 1st Main Rd",
+      "warnings": [],
+      "waypoint_order": []
+    }
+  ],
+  "status": "OK"
+};
+
+// decode polyline and get lat, leg
+function decodePolyline(polyline) {
+
+  var _ = {};
+
+  _.Ya = function(a, b, c) {
+    null != b && (a = Math.max(a, b));
+    null != c && (a = Math.min(a, c));
+    return a
+  };
+  _.Za = function(a, b, c) { c -= b; return ((a - b) % c + c) % c + b };
+  _.w = function(a) { return a ? a.length : 0 };
+
+  _.E = function(a, b, c) {
+    a -= 0;
+    b -= 0;
+    c || (a = _.Ya(a, -90, 90), 180 != b && (b = _.Za(b, -180, 180)));
+    this.lat = function() { return a };
+    this.lng = function() { return b }
+  };
+
+  function decodePath(a) {
+    for (var b = _.w(a), c = Array(Math.floor(a.length / 2)), d = 0, e = 0, f = 0, g = 0; d < b; ++g) {
+      var h = 1,
+        l = 0,
+        n;
+      do n = a.charCodeAt(d++) - 63 - 1, h += n << l, l += 5; while (31 <= n);
+      e += h & 1 ? ~(h >> 1) : h >> 1;
+      h = 1;
+      l = 0;
+      do n = a.charCodeAt(d++) - 63 - 1, h += n << l, l += 5; while (31 <= n);
+      f += h & 1 ? ~(h >> 1) : h >> 1;
+      c[g] = new _.E(1E-5 * e, 1E-5 * f, !0)
+    }
+    c.length = g;
+    return c
+  }
+
+  var result = decodePath(polyline).map(function(el) {
+      return {lat:Number(el.lat().toFixed(5)),lng:Number(el.lng().toFixed(5))};
+  });
+
+  return result;
+
+}
+
+/**
+ * @use distance between two points
+*/
+function getDistanceFromLatLonInMeter(lat1, lon1, lat2, lon2) {
+  var R = 6371000; // Radius of the earth in meter
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in meter
+  return d;
+}
+
+/**
+ * @use get next point at distance of 50 meter
+ */
+function getNextPointOn50Meter(lat1, lng1, lat2, lng2){   
+  var dLon = deg2rad(lng2 - lng1);
+
+  // Find the bearing from this point to the next.
+  var brng = Math.atan2(Math.sin(dLon) * Math.cos(deg2rad(lat2)),
+    Math.cos(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) -
+    Math.sin(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.cos(dLon));
+
+  var R = 6371000;
+  // var brng = 1.57 //Bearing is 90 degrees converted to radians.
+  var d = 50 //Distance in meter
+
+  var latRad = deg2rad(lat1); //Current lat point converted to radians
+  var lngRad = deg2rad(lng1); //Current long point converted to radians
+
+  var lat2Rad = Math.asin( Math.sin(latRad)*Math.cos(d/R) +
+    Math.cos(latRad)*Math.sin(d/R)*Math.cos(brng));
+
+  var lng2Rad = lngRad + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(latRad),
+    Math.cos(d/R)-Math.sin(latRad)*Math.sin(lat2Rad));
+
+  return {
+    lat: rad2deg(lat2Rad),
+    lng: rad2deg(lng2Rad)
+  };
+}
+
+/**
+ * @use convert degree to radian
+ */
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+/**
+ * @use radian to degree
+ */
+function rad2deg(rad) {
+  return (rad * 180) / Math.PI;// deg * (Math.PI/180)
+}
+
+
+/**
+ * @use get points from polyline to draw on view template
+ */
+function getResultFromPolylines(){
+    // let inputJson = document.getElementById('inputJson').value;
+    // console.log(inputJson, 'INPUT ------------');
+
+    // let parse = JSON.parse(JSON.stringify(inputJson));
+
+    // console.log(parse, 'PARSE-----------------');
+
+    // if(parse !== undefined)
+    //   res = parse;
+    
+    // console.log('res-----', res);
+    /* --------------------------------------------------- */
+
+    let points = [];
+
+    res.routes.forEach(route => {
+      route.legs.forEach(leg => {
+        leg.steps.forEach(step => {
+          points.push(step.polyline.points);
+        })
+      })
+    });
+
+    console.log(points);
+
+    let result = [];
+
+    points.forEach((point, index) => {
+      
+      let decodedPoints = decodePolyline(point);
+      let previous = {...decodedPoints[0]};
+      for(let i=0; i < decodedPoints.length-1;){
+        let distance = 0;
+        let d1 = getDistanceFromLatLonInMeter(previous.lat, previous.lng, decodedPoints[i+1].lat, decodedPoints[i+1].lng);
+        distance += d1;
+        i++;
+        while(distance < 50 && i < decodedPoints.length-1){
+          let d = getDistanceFromLatLonInMeter(decodedPoints[i].lat, decodedPoints[i].lng, decodedPoints[i+1].lat, decodedPoints[i+1].lng);
+          distance += d;
+          i++;
+        }
+
+        if(i === decodedPoints.length-1){
+          // add ith to result set
+          result.push({...previous});
+          // result.push({...decodedPoints[i]});
+        }else{
+          if(result.length == 0 || 
+            (result.length > 0 && getDistanceFromLatLonInMeter(previous.lat, previous.lng, (result[result.length-1]).lat, (result[result.length-1]).lng) >= 25))
+            result.push({...previous});
+          
+            let next50 = getNextPointOn50Meter(previous.lat, previous.lng, decodedPoints[i].lat, decodedPoints[i].lng);
+          previous = next50;
+        }
+      }
+      console.log(result, "_________________________________");
+    });
+
+    resultDataToRepresent = '';
+
+    result.forEach(r => {
+      resultDataToRepresent += `${r.lat},${r.lng},blue,marker<br/>`;
+    });
+
+    document.getElementById('result').innerHTML = resultDataToRepresent;
+}
+
+// ------- invoke
+getResultFromPolylines();
+
+
+// 12.93166,77.62852,blue,marker
+// 12.931249335460466,77.62870793137844,blue,marker
+// 12.930835226843424,77.62888773059389,blue,marker
+// 12.930426268736074,77.62907953863953,blue,marker
+// 12.93001,77.62925,blue,marker
+// 12.929904241884044,77.62969841688381,blue,marker
+// 12.929797730329549,77.63014664580861,blue,marker
+// 12.929696407976943,77.63059613923063,blue,marker
+// 12.92959812372358,77.63104634203049,blue,marker
+// 12.92951780432022,77.63150028047275,blue,marker
+// 12.929448230090875,77.63195608251819,blue,marker
+// 12.929558608793828,77.63240332479891,blue,marker
+// 12.9298,77.63302,blue,marker
+// 12.929854948160154,77.6334779010196,blue,marker
+// 12.9296619640405,77.63389460940648,blue,marker
+// 12.929446734239628,77.63429968470082,blue,marker
+// 12.929257605849678,77.63471824925304,blue,marker
+// 12.929050141615056,77.63512756687255,blue,marker
+// 12.928843639049216,77.63553739575644,blue,marker
+// 12.928630816481965,77.63594380643788,blue,marker
+// 12.928435499279173,77.63635936689874,blue,marker
+// 12.928219385712222,77.63676394456382,blue,marker
+// 12.927915844603213,77.63710432189859,blue,marker
+// 12.927613609682851,77.63744592019648,blue,marker
+// 12.92754,77.63751,blue,marker
+// 12.92722,77.63718,blue,marker
+// 12.92677033919704,77.63718,blue,marker
